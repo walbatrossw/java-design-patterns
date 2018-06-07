2fc8a93b3d51e02870c75d9605da831d12a8fd0a
SangSeob Yoon <walbatrossw@gmail.com>
2018-06-07 23:57:15 +0900

    32nd commit : solid principles 8 (OCP)

---
+++ ch03-solid/src/practice/p01/Bag.java
@@ -0,0 +1,14 @@
+package practice.p01;
+
+public class Bag {
+
+    private int price;
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
