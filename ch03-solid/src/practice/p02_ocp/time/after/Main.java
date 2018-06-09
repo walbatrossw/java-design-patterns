d01ddc3d7d12b20e6052596424394d9346359615
SangSeob Yoon <walbatrossw@gmail.com>
2018-06-09 16:29:17 +0900

    42nd commit : solid principles codes2

--- ch03-solid/src/practice/p02_ocp/time/after/Main.java
+++ ch03-solid/src/practice/p02_ocp/time/after/Main.java
@@ -1,4 +1,11 @@
 package practice.p02_ocp.time.after;
 
 public class Main {
+
+    public static void main(String[] args) {
+        TimeReminder timeReminder = new TimeReminder();
+        TimeProvider timeProvider = new FakeTimeProvider();
+        timeProvider.setHours(18);
+        timeReminder.setTimeProvider(timeProvider);
+    }
 }
